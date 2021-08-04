5dd06850e654259c5a47927ee959eb14c74a0cc5
Alex Dom <devalexdom@gmail.com>
2021-08-04 17:17:57 +0200

    Fix TypeScript file referency

--- core-v2.x/rollup.config.js
+++ core-v2.x/rollup.config.js
@@ -1,37 +1,21 @@
 import typescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs';
 
 export default [
     // ES Modules
     {
-        input: 'src/jsvanillahelper-core.ts',
+        input: 'src/index.ts',
         output: [{
             file: 'dist/index.js', format: 'cjs',
-        },
-        {
-            file: 'dist/index.es.js', format: 'es',
         }
         ],
         plugins: [
+            commonjs(),
             typescript({ useTsconfigDeclarationDir: true }),
             babel({ extensions: ['.ts'] }),
         ],
     },
 
-    // UMD
-    /*{
-        input: 'src/jsvanillahelper-core.ts',
-        output: {
-            file: 'dist/index.umd.min.js',
-            format: 'umd',
-            name: 'index',
-            indent: false,
-        },
-        plugins: [
-            typescript(),
-            babel({ extensions: ['.ts'], exclude: 'node_modules/**' }),
-            terser(),
-        ],
-    },*/
 ]
